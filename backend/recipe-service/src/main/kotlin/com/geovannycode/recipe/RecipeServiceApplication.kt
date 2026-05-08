e1930a2589edf08baa3064fa0193c08ab45575c2
Geovanny Mendoza <me@geovannycode.com>
2026-05-07 21:50:27 -0500

    feat(recipe-service): initialize project structure, build config and application.yaml

---
+++ backend/recipe-service/src/main/kotlin/com/geovannycode/recipe/RecipeServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.geovannycode.recipe
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RecipeServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<RecipeServiceApplication>(*args)
+}
